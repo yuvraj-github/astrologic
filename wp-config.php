86c8b397ab62ae1d3e5d7b4ed3d897b7433d8cd0
Yuv Raj Subh <54491015+yuvraj-github@users.noreply.github.com>
2022-01-15 10:29:01 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'u905095300_astrologic' );
+define( 'DB_NAME', 'u905095300_astrologi' );
 
 /** MySQL database username */
 define( 'DB_USER', 'u905095300_astrologic' );
